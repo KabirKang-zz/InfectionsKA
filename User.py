d902470f7228b92c9668d8e22d33904928ae6734
Kabir <kangkabir@gmail.com>
2015-02-08 22:28:06 -0800

    Added methods to add coaches, students and infect

--- User.py
+++ User.py
@@ -1,10 +1,19 @@
 __author__ = 'Kabir'
 # Defines a superclass for users of a network-like service
 class User:
-    def __init__(self, userName = "User"):
-        self.userName = userName
+    def __init__(self, name = "User"):
+        self.name = name
         self.coaches = []
         self.students = []
         self.infected = False
-        def infect(self):
-            self.infected = True
+    def __str__(self):
+        if(self.infected):
+            return "{} is infected.".format(self.name)
+        else:
+            return "{} is not infected.".format(self.name)
+
+    def infect(self):
+        self.infected = True
+    def addCoach(self,user):
+        self.students.append(user)
+        user.coaches.append(self)
