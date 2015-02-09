36cca001e10e8808c474cf596ab2f826269bb673
Kabir <kangkabir@gmail.com>
2015-02-08 23:34:39 -0800

    User class apart from infection spread methods is more or less complete

--- User.py
+++ User.py
@@ -15,11 +15,22 @@ class User:
     def infect(self):
         self.infected = True
     def addCoach(self,user):
+        # Need to include check that user not in list yet
         user.students.append(self)
         self.coaches.append(user)
+
+    def addStudent(self,user):
+        self.students.append(user)
+        user.coaches.append(self)
 
     def printStudents(self):
         students = []
         for s in self.students:
             students.append(s.name)
         print "{} has students: {}".format(self.name, students)
+
+    def printCoaches(self):
+        coaches = []
+        for c in self.coaches:
+            coaches.append(c.name)
+        print "{} has coaches: {}".format(self.name, coaches)
