982ea576d43e1dd013cf3e1dab107bf4b92aa9ad
Kabir <kangkabir@gmail.com>
2015-02-10 12:08:13 -0800

    Recursive TotalInfection works

--- Infection.py
+++ Infection.py
@@ -4,17 +4,40 @@ from User import User
 
 def totalInfection(focus):
     focus.infect()
+    for c in focus.coaches:
+        spread(focus, c)
+
+    for s in focus.students:
+        spread(focus, s)
 
 # Infects everyone who has a proximal or distant connection to focus
-def spread(self):
-    pass
+def spread(spreadFrom, spreadTo):
+    # recursive spread
+    spreadTo.infect()
+    for c in spreadTo.coaches:
+        if c != spreadFrom:
+            spread(spreadTo,c)
+    for s in spreadTo.students:
+        if s != spreadFrom:
+            spread(spreadTo,s)
 
 def main():
     kabir = User("Kabir")
+    a = User("a")
+    b = User("b")
+    c = User("c")
+    d = User("d")
+    e = User("e")
+    f = User("f")
+    g = User("g")
+    kabir.addCoaches([a,b,c])
+    kabir.addStudents([d,e,f])
     totalInfection(kabir)
-    print kabir.infected
-    kabir.disinfect()
-    print kabir.infected
+    print a.infected
+    print b.infected
+    print d.infected
+    print g.infected
+
 
 if __name__ == "__main__":
     main()
