ce94dcc8355e61ff4c6a1f9d1fc4433c3dd761b3
Kabir <kangkabir@gmail.com>
2015-02-10 16:41:21 -0800

    Comments added

--- Infection.py
+++ Infection.py
@@ -3,8 +3,7 @@ import Test
 # Starting from any given user, the entire connected component of the coaching graph
 # containing that user should become infected.
 
-#totalAffected = 1 # start by infecting focus
-
+# Total infection method recurses through every node infecting everyone
 def total_infection(focus):
     focus.infect()
     for c in focus.coaches:
@@ -12,22 +11,8 @@ def total_infection(focus):
 
     for s in focus.students:
         spread(focus, s)
-"""
-def limited_infection(focus, targetNum):
 
-    focus.infect()
-    for c in focus.coaches:
-        if targetNum>totalAffected:
-            spread(focus, c)
-        else:
-            return
-
-    for s in focus.students:
-        if targetNum>totalAffected:
-            spread(focus, s)
-"""
-
-# Infects everyone who has a proximal or distant connection to focus
+# Called by total_infection. Infects everyone who has a proximal or distant connection to focus
 def spread(spreadFrom, spreadTo):
     # recursive spread
     spreadTo.infect()
@@ -39,50 +24,36 @@ def spread(spreadFrom, spreadTo):
         if s != spreadFrom:
             spread(spreadTo,s)
 
+# This method sorts a user's students by the number of students they have and coaches by their number of coaches.
+# It infects smaller subtrees to ensure that the number gets as close to the target as possible
 def limited_infection(focus, targetNum):
-    # The basic and most common case is a teacher that has many courses
-    focus.infect()
+    if targetNum>User.population:
+        exit("The population is not even that high!")
 
     if User.total_infected>=targetNum:
         return
 
-    classroom = []
-    for student in focus.students:
-        classroom.append((len(student.students),student))
+    else:
+        focus.infect()
 
-    classroom.sort() # Sort students by their number of students
+        classroom = []
+        for student in focus.students:
+            classroom.append((len(student.students),student))
 
-    for tup in classroom:
-        limited_infection(tup[1], targetNum-1)
+        classroom.sort() # Sort students by their number of students
 
-    coaches = []
-    for coach in focus.coaches:
-        coaches.append((len(coach.coaches),coach))
+        for tup in classroom:
+            limited_infection(tup[1], targetNum-1)
 
-    coaches.sort()
+        coaches = []
+        for coach in focus.coaches:
+            coaches.append((len(coach.coaches),coach))
 
-    for tup in coaches:
-        limited_infection(tup[1], targetNum-1)
+        coaches.sort()  # Sort coaches by their number of coaches
 
-    """for c in focus.coaches:
-        if targetNum>totalAffected:
-            spread_limited(focus, c)
-        else:
-            return
+        for tup in coaches:
+            limited_infection(tup[1], targetNum-1)
 
-    for s in focus.students:
-        if targetNum>totalAffected:
-            spread_limited(focus, s)
-"""
-def spread_limited(spreadFrom, spreadTo):
-
-    spreadTo.infect()
-    for c in spreadTo.coaches:
-        if c != spreadFrom:
-            spread_limited(spreadTo,c)
-    for s in spreadTo.students:
-        if s != spreadFrom:
-            spread_limited(spreadTo,s)
 
 def main():
     Test.test()
